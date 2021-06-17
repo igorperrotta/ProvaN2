39fb028f2cdd93b5eabe711261d98dcf6b73ea4a
IgorPerrotta <>
2021-06-17 23:49:10 +0000

    Initial commit

---
+++ main.scm
@@ -0,0 +1,3 @@
+(define (calculaSaco umCentavo cincoCentavos dezCentavos vinteECinco cinquenta umReal )
+  (/ (+ (* umCentavo 1) (* cincoCentavos 5) (* dezCentavos 10 ) (* vinteECinco 25) (* cinquenta 50) (* umReal 100)) 100)
+)
